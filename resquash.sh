e595c0495999b9fbf100c44188c2264243db0e5b
Pete <pete3n@protonmail.com>
2024-01-14 20:02:53 -0500

    Fixed bootable ISO

--- resquash.sh
+++ resquash.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p squashfsTools xorriso syslinux
+#! nix-shell -i bash -p squashfsTools xorriso cdrtools syslinux
 
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run with sudo privileges."
@@ -98,24 +98,27 @@ echo "Copying new nix-store SquashFS..."
 cp $MODIFIED_SQUASHFS_PATH $NEW_ISO_PATH/$(basename $SQUASHFS_PATH)
 echo
 
-FULL_PATH_TO_ISOLINUX_BIN=$(realpath $NEW_ISO_PATH/isolinux/isolinux.bin)
-echo "Full path to isolinux.bin: $FULL_PATH_TO_ISOLINUX_BIN"
+ls -la $NEW_ISO_PATH
 
-echo "Creating new hybrid bootable ISO file..."
-xorriso -as mkisofs -o $NEW_ISO_FILE -J -R \
-    -b isolinux/isolinux.bin -c .boot.cat \
-    -no-emul-boot -boot-load-size 4 -boot-info-table \
-    -eltorito-alt-boot -e /EFI/boot/bootx64.efi -no-emul-boot \
-    -append_partition 2 0xef $NEW_ISO_PATH/boot/efi.img \
+echo "Creating new bootable ISO file..."
+xorriso -as mkisofs -isohybrid-mbr $NEW_ISO_PATH/isolinux/isohdpfx.bin -c isolinux/boot.cat -b isolinux/isolinux.bin \
+    -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot \
+    -e boot/efi.img -no-emul-boot -isohybrid-gpt-basdat \
+    -o $NEW_ISO_FILE \
     -graft-points \
-        /isolinux=$NEW_ISO_PATH/isolinux \
-        /EFI=$NEW_ISO_PATH/EFI \
-        /=$NEW_ISO_PATH
+    /isolinux=$NEW_ISO_PATH/isolinux \
+    /EFI=$NEW_ISO_PATH/EFI \
+    $NEW_ISO_PATH
 echo
 
-echo "Making bootable..."
-isohybrid --uefi $NEW_ISO_FILE
-echo
+#-graft-points \
+#        /isolinux=$NEW_ISO_PATH/isolinux \
+#        /EFI=$NEW_ISO_PATH/EFI \
+#        /=$NEW_ISO_PATH
+
+#echo "Making bootable..."
+#isohybrid --uefi $NEW_ISO_FILE
+#echo
 
 echo "Unmounting old ISO and removing temp files..."
 umount $MOUNT_POINT
